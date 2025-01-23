4ef967b36c7885486c56956f0068dcc760d1dde2
Nagatomo <reiji128@icloud.com>
2025-01-23 22:47:08 +0900

    Revert "設定の追加"
    This reverts commit b5539c8fb80578f9077ba75f57145f8ec7db7e4d.

--- Network/Source/Network/Network.Build.cs
+++ Network/Source/Network/Network.Build.cs
@@ -8,7 +8,7 @@ public class Network : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "OnlineSubsystemSteam", "OnlineSubsystem"});
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
